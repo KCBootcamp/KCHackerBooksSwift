dd97bb3119a1f5394066399410e61b2cf8b4d400
Bhavish Chandnani <bhavish.chandnani@t-systems.com>
2016-07-13 14:37:37 +0200

    Created classes for Constants and errors

---
+++ KCHackerBooks/BVCHBooksErrors.swift
@@ -0,0 +1,19 @@
+//
+//  Errors.swift
+//  KCHackerBooks
+//
+//  Created by Bhavish Chandnani on 10/7/16.
+//  Copyright © 2016 BVC. All rights reserved.
+//
+
+import Foundation
+enum BVCHackersBookErrors : ErrorType {
+    case wrongURLFormatForJSONResource
+    case resourcePointedByURLNotReachable
+    case jsonParsingError
+    case wrongJSONFormat
+    case nilJSONObject
+    case wrongPath
+    case urlConversionError
+    case downloadError
+}
---
+++ KCHackerBooks/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  KCHackerBooks
+//
+//  Created by Bhavish Chandnani on 11/7/16.
+//  Copyright © 2016 BVC. All rights reserved.
+//
+
+import Foundation
+
+enum UserDefaultKeys: String{
+    case jsonFile = "json"
+}
+
+enum RelativePathValues: String {
+    case imagesPath = "images/"
+    case pdfsPath = "pdfs/"
+}
+
+enum FilesName: String {
+    case booksFile = "books_readable.json"
+}
+
+enum DownloadUrls:  String {
+    case jsonUrl = "https://t.co/K9ziV0z3SJ"
+}
