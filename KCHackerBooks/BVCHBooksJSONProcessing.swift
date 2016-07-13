6eb41b37d1bd2239477a06f8340e0c7ada59ec48
Bhavish Chandnani <bhavish.chandnani@t-systems.com>
2016-07-13 14:39:30 +0200

    Created Json Processing class to decode JsonArray to an object

---
+++ KCHackerBooks/BVCHBooksJSONProcessing.swift
@@ -0,0 +1,100 @@
+//
+//  BVCBooksJSONProcessing.swift
+//  KCHackerBooks
+//
+//  Created by Bhavish Chandnani on 10/7/16.
+//  Copyright © 2016 BVC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+//{
+//    "authors": "Scott Chacon, Ben Straub",
+//    "image_url": "http://hackershelf.com/media/cache/b4/24/b42409de128aa7f1c9abbbfa549914de.jpg",
+//    "pdf_url": "https://progit2.s3.amazonaws.com/en/2015-03-06-439c2/progit-en.376.pdf",
+//    "tags": "version control, git",
+//    "title": "Pro Git"
+//}
+
+//MARK: - Aliases
+typealias JSONObject        =   AnyObject
+typealias JSONDictionary    =   [String : JSONObject]
+typealias JSONArray         =   [JSONDictionary]
+
+//MARK: - Decoding
+func decode (hackerBook  json: JSONDictionary) throws -> BVCBook{
+    
+    //Validation
+    
+    //TODO: check and modify
+
+//    guard let imageUrlStr = imageStr,
+//        imageUrl = NSURL(string:(imageUrlStr)),
+//        imageData = NSData(contentsOfURL:imageUrl),
+//        image = UIImage (data: imageData) else{
+//            throw BVCHackersBookErrors.resourcePointedByURLNotReachable
+//    }
+    let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+    
+    guard let imageStr = json["image_url"] as? String,
+        imageUrlStr =  (NSURL (fileURLWithPath: documentsPath).URLByAppendingPathComponent(imageStr)).path,
+        image = UIImage(named: imageUrlStr)
+    else{
+        throw BVCHackersBookErrors.resourcePointedByURLNotReachable
+    }
+
+    guard let pdfStr = json["pdf_url"] as? String,
+        urlStr = (NSURL (fileURLWithPath: documentsPath).URLByAppendingPathComponent(pdfStr)).path, url = NSURL(string: urlStr) else{
+        throw BVCHackersBookErrors.wrongURLFormatForJSONResource
+    }
+    
+    guard let authors = json["authors"] as? String,
+    authorsArray : [String]? = convertStringToArray(authors, separator: ", ") else {
+            throw BVCHackersBookErrors.wrongJSONFormat
+    }
+    
+    guard let tags = json["tags"] as? String,
+        tagsArray : [String]? = convertStringToArray(tags, separator: ", ")
+        else {
+            throw BVCHackersBookErrors.wrongJSONFormat
+    }
+    
+    if let title = json["title"] as? String {
+        return BVCBook(title: title, authors: authorsArray, tags: tagsArray, image: image, pdfURL: url)
+    }else{
+        throw BVCHackersBookErrors.wrongJSONFormat
+    }
+    
+
+}
+//
+//func decode(hackerBook json: JSONDictionary?, modifyURLs: Bool) throws -> BVCBook{
+//    
+//    
+//    if case .Some(let jsonDict) = json{
+//        return try decode(hackerBook: jsonDict)
+//    }else{
+//        throw BVCHackersBookErrors.nilJSONObject
+//    }
+//}
+
+//MARK: - Loading
+func loadFromLocalFile(fileName name: String, bundle: NSBundle = NSBundle.mainBundle()) throws -> JSONArray{
+    
+    if let url = bundle.URLForResource(name),
+        data = NSData(contentsOfURL: url),
+        maybeArray = try? NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as? JSONArray,
+        array = maybeArray{
+        
+        return array
+        
+    }else{
+        throw BVCHackersBookErrors.jsonParsingError
+    }
+}
+
+
+//MARK: - Utilities
+func convertStringToArray (string : String, separator: String) -> [String]{
+    return string.componentsSeparatedByString(separator)
+}
